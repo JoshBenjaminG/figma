f1ee79dd013232792323d3d07dad73e2dc924d7e
JoshG <93669172+JoshBenjaminG@users.noreply.github.com>
2022-09-06 00:05:48 -0500

    Add files via upload

---
+++ figma-conversion/components/header_23.php
@@ -0,0 +1,32 @@
+
+
+	
+	<body>
+		<section class='site-header'>
+		<inner-column>
+
+			<header class="header_23">
+
+				<nav class="logo">Logoipsum</nav>
+
+				<nav class='links'>
+					<a href="">Home</a>
+					<a href="">Updates</a>
+					<a href="">Services</a>
+					<a href="">Features</a>
+					<a href="">About Us</a>
+				</nav>
+
+				<nav class='links'>
+					<a href="">EN</a>
+					<a href="">Login</a>
+				</nav>
+
+			</header>
+
+		</inner-column>
+		</section>
+
+
+		<main class='page-content'>
+			<!-- STUFF -->
